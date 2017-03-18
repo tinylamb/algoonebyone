5c67b855794cf75d0dbe08de838a77aaf35ed28a
samo.gy <samo.gy@alibaba-inc.com>
2017-03-18 08:10:50 +0800

    binarysearch

--- src/test/java/qwer/CaseTest.java
+++ src/test/java/qwer/CaseTest.java
@@ -1,15 +1,59 @@
 package qwer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaseTest {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		testBinarySearch();
+//		testBinarySearch();
+		testBS2();
 	}
 	
 	public static void testBinarySearch() {
 		int v = 9;
 		int[] arr = {1, 4, 6, 9, 11, 102, 111, 222, 333};
 		System.out.println( Test.binarySearch(v, arr));
+	}
+	
+	public static void testBS() {
+		Double t1 = -1d;
+		Double t2 = 2d;
+		Double t3 = 5d;
+		Double t4 = 6d;
+		Double t5 = 9d;
+		Double t6 = 100d;
+		Double t7 = 151d;
+		List<Double> interval = new ArrayList<Double>();
+		interval.add(2d);
+		interval.add(6d);
+		interval.add(100d);
+		System.out.println(t1 + " : " + Test.binarySearch(t1, interval));
+		System.out.println(t2 + " : " + Test.binarySearch(t2, interval));
+		System.out.println(t3 + " : " + Test.binarySearch(t3, interval));
+		System.out.println(t4 + " : " + Test.binarySearch(t4, interval));
+		System.out.println(t5 + " : " + Test.binarySearch(t5, interval));
+		System.out.println(t6 + " : " + Test.binarySearch(t6, interval));
+		System.out.println(t7 + " : " + Test.binarySearch(t7, interval));
+	}
+	
+	public static void testBS2() {
+		List<Double> interval = new ArrayList<Double>();
+		interval.add(-1d);
+		interval.add(3d);
+		Double t1 = -5d;
+		Double t2 = -1d;
+		Double t3 = 2d;
+		Double t4 = 3d;
+		Double t5 = 8d;
+		
+		System.out.println(t1 + " : " + Test.binarySearch(t1, interval));
+		System.out.println(t2 + " : " + Test.binarySearch(t2, interval));
+		System.out.println(t3 + " : " + Test.binarySearch(t3, interval));
+		System.out.println(t4 + " : " + Test.binarySearch(t4, interval));
+		System.out.println(t5 + " : " + Test.binarySearch(t5, interval));
+
+		
 	}
 }
--- src/test/java/qwer/Test.java
+++ src/test/java/qwer/Test.java
@@ -1,18 +1,56 @@
 package qwer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
-	
+    
 	public static void main(String[] args) {
-		
+		Double t = 101d;
+		List<Double> interval = new ArrayList<Double>();
+		interval.add(2d);
+		interval.add(6d);
+		interval.add(100d);
+		System.out.println(binarySearch(t, interval));
 	}
 	
-	public static int getGCD(int p, int q) {
+    public static int getGCD(int p, int q) {
 		if(q == 0) {
 			return p;
 		} else {
 			return getGCD(q, p % q);
 		}
 	}
+    
+    public static int binarySearch(Double fval, List<Double> interval) {
+        if(interval.size() == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = interval.size() - 1;
+        int mid = left + ((right - left) >> 1);
+        int pos = 0;
+        Double tmp;
+        while(left != right) {
+            mid = left + ((right - left) >> 1);
+            tmp = interval.get(mid);
+            if(fval == tmp) {
+                return mid + 1;
+            } else if (fval > tmp) {
+                left = mid + 1;
+            } else {
+//                right = mid - 1;
+            	right = mid;
+            }
+
+        }
+        mid = right;
+        if(fval > interval.get(mid)) {
+            return mid + 2;
+        } else {
+            return mid + 1;
+        }
+    }
 	
 	public static int binarySearch(int val, int[] arr) {
 		if(arr.length == 0) {
