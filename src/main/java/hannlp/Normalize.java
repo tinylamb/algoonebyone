661c9ba05bc4883036b0fd0ed5d27909914d2490
samo.gy <samo.gy@alibaba-inc.com>
2017-07-04 17:04:15 +0800

    update

--- src/main/java/hannlp/Normalize.java
+++ src/main/java/hannlp/Normalize.java
@@ -20,4 +20,24 @@ public class Normalize {
         }
         return new String(c);
     }
+
+    private static String keepChi(String input) {
+        char[] c = input.toCharArray();
+        int len = 0;
+        for (int i = 0; i < c.length; i++) {
+            //if (Character.toString(c[i]).matches("[\u4E00-\u9FA5]+")) {
+            //    c[len++] = c[i];
+            //}
+            if (c[i] >= 19968 && c[i] <= 40869) {
+                c[len++] = c[i];
+            }
+        }
+        char[] result = new char[len];
+        //for (int j = 0; j < len; j++) {
+        //    result[j] = c[j];
+        //}
+        System.arraycopy(c, 0, result, 0, len);
+        return new String(result);
+
+    }
 }
