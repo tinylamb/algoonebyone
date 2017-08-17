348ef9e77746ef47f28e72aa39fd1b33e6c7117f
samo.gy <samo.gy@alibaba-inc.com>
2017-08-17 11:12:45 +0800

    +update

--- src/main/java/algsexcise/RightTriangle.java
+++ src/main/java/algsexcise/RightTriangle.java
@@ -1,10 +1,25 @@
 package algsexcise;
 
-/**
- * Created by samo on 2017/8/17.
+import algs4.StdDraw;
+
+/******************************************************************************
+ *  Compilation:  javac RightTriangle.java
+ *  Execution:    java RightTriangle
+ *  Dependencies: StdDraw.java
  *
- * @author samo
- * @date 2017/08/17
- */
+ *  Draws a right triangle and a circumscribing circle.
+ *
+ ******************************************************************************/
+
 public class RightTriangle {
+
+    public static void main(String[] args) {
+        StdDraw.square(.5, .5, .5);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.line(.5, .5, .9, .5);
+        StdDraw.line(.9, .5, .5, .8);
+        StdDraw.line(.5, .8, .5, .5);
+        StdDraw.circle(.7, .65, .25);
+    }
 }
+
