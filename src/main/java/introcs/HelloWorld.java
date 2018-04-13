26e0aa6b7677d87441fc0f1537a17fb523e23ccc
samo.gy <samo.gy@alibaba-inc.com>
2018-04-13 16:51:07 +0800

    update

---
+++ src/main/java/introcs/HelloWorld.java
@@ -0,0 +1,23 @@
+package introcs;
+
+/**
+ * Created by samo on 2018/4/13.
+ *
+ * @author samo
+ * @date 2018/04/13
+ */
+public class HelloWorld {
+
+    public HelloWorld() {
+        System.out.println("HelloWorld Constructor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        /**
+         * javac HelloWorld.java   生成   HelloWorld.class
+         * javap -c HelloWorld.class
+         * cd ..;java introcs.HelloWorld
+         */
+    }
+}
